6aa2280247d135fd56d3ff4c6d1360c0e6cd6814
Jenn J Will <jennijwill@gmail.com>
2019-07-23 16:35:42 -0700

    HW questions completed except last one

---
+++ commands.js
@@ -0,0 +1,71 @@
+const fs = require("fs");
+
+function done(output) {
+  process.stdout.write(output);
+  process.stdout.write("\nprompt > ");
+}
+
+function evaluateCmd(userInput) {
+  const userInputArray = userInput.split(" ");
+  const command = userInputArray[0];
+
+  switch (command) {
+    case "echo":
+      commandLibrary.echo(userInputArray.slice(1).join(" "));
+      break;
+    case "cat":
+      commandLibrary.cat(userInputArray.slice(1));
+      break;
+    case "head":
+      commandLibrary.head(userInputArray.slice(1));
+      break;
+    case "tail":
+      commandLibrary.tail(userInputArray.slice(1));
+    default:
+      process.stdout.write(
+        "You're an awesome coder but that command doesn't exist. Please try another."
+      );
+  }
+}
+
+const commandLibrary = {
+  echo: function(userInput) {
+    done(userInput);
+  },
+  cat: function(fullPath) {
+    const fileName = fullPath[0];
+    fs.readFile(fileName, (err, data) => {
+      if (err) throw err;
+      done(data);
+    });
+  },
+  head: function(fullPath) {
+    const fileName = fullPath[0];
+    fs.readFile(fileName, (err, data) => {
+      if (err) throw err;
+      let text = data.toString("utf8");
+      let slicedText = text
+        .split("\n")
+        .slice(0, 5) //1st 5 lines
+        .join("\n");
+      let bufferText = Buffer.from(slicedText, "utf8");
+      done(bufferText);
+    });
+  },
+  tail: function(fullPath) {
+    const fileName = fullPath[0];
+    fs.readFile(fileName, (err, data) => {
+      if (err) throw err;
+      let text = data.toString("utf8");
+      let slicedText = text
+        .split("\n")
+        .slice(-3) //last 3 lines
+        .join("\n");
+      let bufferText = Buffer.from(slicedText, "utf8");
+      done(bufferText);
+    });
+  }
+};
+
+module.exports.commandLibrary = commandLibrary;
+module.exports.evaluateCmd = evaluateCmd;
